1d22341a730d936143fbf7efe904f30b0547599c
Tamiris Beira <tamirisbeira@gmail.com>
2023-08-10 15:36:54 -0300

    correção cors

--- api/src/server.js
+++ api/src/server.js
@@ -1,4 +1,9 @@
 require('dotenv/config');
+const cors = require('cors');
 import app from './app';
+
+app.use(cors({
+    origin: 'https://todo-list-woad-gamma.vercel.app'
+}));
 
 app.listen(process.env.PORT);
