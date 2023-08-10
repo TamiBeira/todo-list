1538ff44c39728d59322b2437a97c67087060522
Tamiris Beira <tamirisbeira@gmail.com>
2023-08-10 17:45:47 -0300

    add build 5

--- api/build.sh
+++ api/build.sh
@@ -2,5 +2,9 @@
 # exit on error
 set -o errexit
 
-yarn
+# Instala as dependências do projeto
+yarn install
 yarn build
+
+# Caso queira rodar as migrations durante o processo de construção
+npx sequelize-cli db:migrate
