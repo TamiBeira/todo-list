75f37b1d7a3dff194628b5aadb62ad1f667c4d29
Tamiris Beira <tamirisbeira@gmail.com>
2023-08-10 13:27:50 -0300

    correção bug

--- api/src/config/database.js
+++ api/src/config/database.js
@@ -1,26 +1,28 @@
 import 'dotenv/config';
 
-
 const nodeEnv = process.env.NODE_ENV;
+
 if (nodeEnv === "production") {
-return {
-dialect: "postgres",
-url: process.env.DATABASE_URL,
-//entities: [entitiesPath],
-//migrations: [migrationsPath],
-};
-}
-
-
-module.exports = {
+  module.exports = {
+    dialect: "postgres",
+    url: process.env.DATABASE_URL,
+    define: {
+      timestamps: false,
+      underscored: true,
+      underscoredAll: true
+    }
+  };
+} else {
+  module.exports = {
     dialect: process.env.DB,
     host: process.env.DB_HOST,
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    define:{
-        timestamps: false,
-        underscored: true,
-        underscoredAll: true
+    define: {
+      timestamps: false,
+      underscored: true,
+      underscoredAll: true
     }
+  };
 }
