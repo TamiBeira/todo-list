28ae573f6787f79ee2f1f5ca2af771aa8af6f5a6
Tamiris Beira <tamirisbeira@gmail.com>
2023-08-25 18:21:19 -0300

    Atualizando cors

--- api/src/config/database.js
+++ api/src/config/database.js
@@ -1,34 +1,18 @@
 import 'dotenv/config';
 
 const nodeEnv = process.env.NODE_ENV;
-
-if (nodeEnv === "production") {
-  module.exports = {
-    dialect: 'postgres',
-    url: process.env.DATABASE_URL,
-    define: {
-      timestamps: false,
-      underscored: true,
-      underscoredAll: true
-    },
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
+module.exports = {
+  dialect: 'postgres',
+  url: process.env.DATABASE_URL,
+  define: {
+    timestamps: false,
+    underscored: true,
+    underscoredAll: true
+  },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
-  };
-} else {
-  module.exports = {
-    dialect: process.env.DB,
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    define: {
-      timestamps: false,
-      underscored: true,
-      underscoredAll: true
-    }
-  };
-}
+  }
+};
