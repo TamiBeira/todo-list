51cc579f1d41c1435817ce996e793e2efc4a1d32
Tamiris Beira <tamirisbeira@gmail.com>
2023-08-25 17:12:02 -0300

    Atualizando cors

--- api/src/app/middlewares/auth.js
+++ api/src/app/middlewares/auth.js
@@ -1,22 +1,27 @@
 import jwt from 'jsonwebtoken';
-import {promisify} from 'util';
+import { promisify } from 'util';
 import authConfig from '../../config/auth';
 
-
-export default  async (req, res, next) =>{
+export default async (req, res, next) => {
     const authHeader = req.headers.authorization;
 
-    if(!authHeader){
-        return res.status(401).json({error: 'Token não existe!'})
+    // Verifica se o cabeçalho de autorização está presente
+    if (!authHeader) {
+        return res.status(401).json({ error: 'Token não fornecido!' });
     }
 
-    const [, token] = authHeader.split(' ')
-    try{
-        const decode = await promisify(jwt.verify)(token, authConfig.secret)
-        req.userId = decode.id;
+    // Extrai o token do cabeçalho de autorização
+    const [, token] = authHeader.split(' ');
+
+    try {
+        // Verifica a validade do token
+        const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+        // Anexa o ID do usuário ao objeto de solicitação para uso posterior
+        req.userId = decoded.id;
+
         return next();
-    }catch (err){
-        return res.status(401).json({error: 'Token não inválido!'})
+    } catch (err) {
+        return res.status(401).json({ error: 'Token inválido!' });
     }
-
-}
+};
